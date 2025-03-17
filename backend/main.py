f7b41dd853813c57a308eb4acd92f767257c2f43
CassperW <wangyj25@outlook.com>
2025-03-17 13:37:45 +0800

    update

--- backend/main.py
+++ backend/main.py
@@ -15,8 +15,6 @@ async def lifespan(app: FastAPI):
     global knn_model
     knn_model = joblib.load("knn_model_new.joblib")
     print("Model loaded successfully!")
-    # global df
-    # df = pd.read_excel('./data/signdata.xlsx', header=None)
     yield
     knn_model = None
 
@@ -24,10 +22,10 @@ app = FastAPI(lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-     allow_origins=["http://localhost:8080","http://127.0.0.1:8080"],  # 允许的前端地址
-    allow_credentials=True,                  # 是否允许携带 Cookie
-    allow_methods=["*"],                     # 允许的 HTTP 方法
-    allow_headers=["*"],                     # 允许的请求头
+     allow_origins=["http://localhost:8080","http://127.0.0.1:8080"],  
+    allow_credentials=True,                  
+    allow_methods=["*"],                    
+    allow_headers=["*"],                     
 )
 
 @app.get("/predict")
@@ -67,14 +65,11 @@ def predict():
         print(len(array))
         array = array[1:900]
 
-        # 将用户输入的数据转换为 NumPy 数组
         input_data = np.array(array).reshape(1, -1)
 
-        # 使用加载的模型进行预测
         prediction = knn_model.predict(input_data)
         print(prediction)
 
-        # 返回预测结果
         return {"message": "success", "prediction": prediction.tolist()}
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Error during prediction: {str(e)}")
@@ -86,24 +81,6 @@ def test():
     random_number.append(random.randint(7, 11))
     print(random_number)
     return {"message": "success", "prediction": random_number}
-
-# class PredictionRequest(BaseModel):
-#     data: int
-#
-# @app.post("/predict")
-# def predict(request: PredictionRequest):
-#     try:
-#         print(request.data)
-#         data = np.array(df)
-#         column = data.shape[1]
-#         X = data[request.data, 0:column - 1]
-#         prediction = knn_model.predict(X.reshape(1, -1))
-#         # input_data = np.array(request.data).reshape(1, -1)
-#         # prediction = knn_model.predict(input_data)
-#         # 返回预测结果
-#         return {"message": "success", "prediction": prediction.tolist()}
-#     except Exception as e:
-#         raise HTTPException(status_code=500, detail=f"Error during prediction: {str(e)}")
 
 
 if __name__ == '__main__':
